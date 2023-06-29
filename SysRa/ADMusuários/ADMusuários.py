8b81e03ced5a18569958e456f2090ef46241032a
Francisco Cauan Almeida Sampaio <cauansampaio257@gmail.com>
2023-06-29 16:37:08 -0300

    vdsgfv

--- SysRa/ADMusuários/ADMusuários.py
+++ SysRa/ADMusuários/ADMusuários.py
@@ -61,7 +61,7 @@ class AdmUsuarios(LoginPage):
         if (pressionar_enter==True):
             escolher.send_keys(Keys.ENTER)
             sleep(1)         
-    
+    '''
     def adicionarusers(self, nome_element, nomesocial_element, email_element, telefone_element, cartao_element, indentificacao_element):
         novo = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[1]/div[3]/span/a/div/button')
         novo.click()
@@ -90,11 +90,11 @@ class AdmUsuarios(LoginPage):
         cartao = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/form/div[1]/div[2]/div[1]/div[2]/div/div[6]/div[2]/input')
         cartao.click()
         cartao.send_keys(cartao_element)
-        '''
+       
         cpf = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/form/div[1]/div[2]/div[1]/div[2]/div/div[4]/div[2]/input')
         cpf.click()
         cpf.send_keys(cpf_element)
-        '''
+    
         novo_cadastro = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/form/div[2]/div/div/div[2]/div/button')
         novo_cadastro.click()
         sleep(2)
@@ -104,7 +104,7 @@ class AdmUsuarios(LoginPage):
         sleep(1)
         
 
-        self.pressionar_seta('down', 2, pressionar_enter=True)
+        self.pressionar_seta(None, 0, pressionar_enter=True)
         
         indentificacao = self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div[2]/form/div[1]/div[2]/div[2]/input')
         indentificacao.click()
@@ -113,7 +113,7 @@ class AdmUsuarios(LoginPage):
         situacao = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/div/div[2]/form/div[1]/div[3]/div[2]/div/div/div[1]/div[2]')))
         situacao.click()
 
-        self.pressionar_seta('down', 1, pressionar_enter=True)
+        self.pressionar_seta(None, 0, pressionar_enter=True)
         
         grupo_acesso = self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div[2]/form/div[2]/div[1]/div[2]/div/div[1]')
         grupo_acesso.click()
@@ -123,7 +123,6 @@ class AdmUsuarios(LoginPage):
         
         curso = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/div/div[2]/form/div[2]/div[2]/div[2]/div/div/div[1]/div[2]')))
         curso.click()
-        sleep(2)
         sleep(2)
 
         self.pressionar_seta(None, 0, pressionar_enter=True)
@@ -141,9 +140,68 @@ class AdmUsuarios(LoginPage):
 
         ok = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[6]/button[1]')
         ok.click()
+    '''
 
+    def editarusuarios(self, nome_element):
+        body = self.driver.find_element(By.XPATH, '/html/body')
+        filtros = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[2]/div[2]/div[1]/form/div/div[1]/div[2]/div/div')
+        filtros.click()
+        sleep(1)
+
+        self.pressionar_seta(None, 0, pressionar_enter=True)
+
+        descricao = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[2]/div[2]/div[1]/form/div/div[2]/div/div[2]/input')
+        descricao.click()
+        descricao.send_keys(nome_element)
+
+        search = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[2]/div[2]/div[1]/form/div/div[3]/span/span/div/div/button')
+        search.click()
+        sleep(3)
+
+        editar = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[2]/div[2]/div[2]/div[1]/div/div/div[2]/div[1]/div[5]/a/span/span/div/div/button')
+        editar.click()
+        sleep(2)
+
+        body.send_keys(Keys.PAGE_DOWN)
+        sleep(2)
+
+        editariden = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[5]/span/span/div/div/button')
+        editariden.click()
+        sleep(2)
+
+        tipo_aluno = self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div[2]/form/div[1]/div[1]/div[2]/div/div/div[1]/div[2]')
+        tipo_aluno.click()
+        sleep(2)
+
+        self.pressionar_seta('down', 1, pressionar_enter=True)
+     
+        curso = self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div[2]/form/div[2]/div[2]/div[2]/div/div/div[1]/div[2]')
+        curso.click()
+        sleep(2)
+
+        self.pressionar_seta(None, 0, pressionar_enter=True)
+
+        salvariden = self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div[3]/div[2]/button')
+        salvariden.click()
+
+        body.send_keys(Keys.PAGE_DOWN)
+        sleep(1)
+
+        salvarperfil = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/form/div[3]/div/button')
+        salvarperfil.click()
+        sleep(2)
+
+        useratu = self.driver.find_element(By.XPATH, '/html/body/div[2]/div/div[6]/button[1]')
+        useratu.click()
+
+        body.send_keys(Keys.PAGE_UP)
+        sleep(1)
+
+        voltar = self.driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div/div[3]/span/a/div/button')
+        voltar.click()
 
 adm_usuarios = AdmUsuarios()
 adm_usuarios.open_website('https://sysra-h.maracanau.ifce.edu.br/login') 
 adm_usuarios.admusuarios()    
-adm_usuarios.adicionarusers('Hans1', 'Vocalista do Blind Guardian', 'cauansampaio244@gmail.com', '85982309576', '023898', '202211242345')   
+#adm_usuarios.adicionarusers('Hans1', 'Vocalista do Blind Guardian', 'cauansampaio240@gmail.com', '85982309576', '023898', '203456')  
+adm_usuarios.editarusuarios('hans')
